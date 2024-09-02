453aacaccf19ac69b0407f38bfa20cba6214fcbc
KathiravansCode <kathiravanprogrammer@gmail.com>
2024-09-02 23:45:51 +0530

    features added

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,22 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form"
 import TaskCard from "./Components/TaskCard";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-
+import { SignedIn, SignedOut, SignInButton, UserButton,useUser,useAuth } from "@clerk/clerk-react";
+import Intro from "./Components/Intro";
 const firebaseUrl='https://todo-app-14261-default-rtdb.asia-southeast1.firebasedatabase.app/'
 function App() {
 let {register,handleSubmit,reset}=useForm(true);
 let [status,setStatus]=useState(true);
 let [todos,setTodos]=useState([]);
-
+let {user}=useUser();
+let {isSignedIn}=useAuth()
 function submitHandler(data){
   let task=data.taskInput;
 
   setStatus(false)
   axios.post(`${firebaseUrl}todos.json`,{
     title:task,
-
+    by:user.firstName
   }).then(()=>{
     fetchTodos()
     setStatus(true);
@@ -50,9 +51,9 @@ useEffect(()=>{
 
   return (
     <>
-    
+    <SignedIn>
     <h1 className="bg-violet-600 text-white text-center text-lg p-3">“Make each day your masterpiece.” ―John Wooden</h1>
-    <div className="mt-2 ml-5">
+    <div className="mt-2 ml-6 rounded-lg">
     <header>
       <SignedOut>
         <SignInButton />
@@ -61,9 +62,10 @@ useEffect(()=>{
         <UserButton />
       </SignedIn>
     </header>
+    
     </div>
     <div className="w-[380px] mt-8 mx-auto p-2">
-      <h1 className="text-lg text-center">Welcome back to task manager!</h1>
+      <h1 className="text-lg text-center">Welcome back to task manager! {isSignedIn? user.firstName:""}</h1>
     <form action="" onSubmit={handleSubmit(submitHandler)} >
       <div className="text-center">
          <input type="text" {...register("taskInput")} className="w-full border p-3 mt-4 rounded-lg" placeholder="Enter the task..e.g.'Learn Coding'" />
@@ -71,9 +73,16 @@ useEffect(()=>{
       <input type="submit" value={status?"Add task":"Submitting.."} className="w-[100px] bg-violet-300 text-violet-900  mt-3 p-2 rounded-lg hover:cursor-pointer" />
     </form>
     </div>
+    <div>
     {
-     todos.map(todocall=> <TaskCard id={todocall.id} title={todocall.title} deleteHandler={deleteHandler}/>)
+     todos.filter(todo=>isSignedIn?todo.by==user.firstName:true).map(todocall=> <TaskCard id={todocall.id} title={todocall.title} deleteHandler={deleteHandler}/>)
     }
+    </div>
+    </SignedIn>
+    <SignedOut>
+    <SignInButton className="mt-2 ml-6 rounded-lg" />
+      <Intro/>
+    </SignedOut>
     </>
   )
 }
---
+++ src/Components/Intro.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <>
+     
+    <div className="w-[560px] mx-auto mt-44">
+      <img src="https://static.vecteezy.com/system/resources/previews/006/685/446/original/illustration-graphic-cartoon-character-of-business-planning-vector.jpg" alt="" className="h-64 w-64 inline rounded-full" />
+      <h1>Welcome to TaskManager!!Manage your work effectively!!</h1>
+    </div>
+    </>
+  )
+}
+
+export default Intro
